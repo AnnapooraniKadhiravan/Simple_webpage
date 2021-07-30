f9b1e8b60661622a11b379ea160c8f9e07ce9f67
AnnapooraniKadhiravan <86883608+AnnapooraniKadhiravan@users.noreply.github.com>
2021-07-30 22:02:44 +0530

    Simple Webpage sections

---
+++ script.js
@@ -0,0 +1,101 @@
+const form = document.getElementById('form');
+const name = document.getElementById('name');
+const orgName = document.getElementById('orgName');
+const phno = document.getElementById('phno');
+const ext = document.getElementById('ext');
+const email = document.getElementById('email');
+const subject = document.getElementById('subject');
+const message = document.getElementById('message');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	// trim to remove the whitespaces
+	const nameValue = name.value.trim();
+	const orgNameValue = orgName.value.trim();
+	const phnoValue = phno.value.trim();
+	const extValue = ext.value.trim();
+    const emailValue = email.value.trim();
+    const subjectValue = subject.value.trim();
+    const messageValue = message.value.trim();
+	
+	if(nameValue === '') {
+		//show error message
+        //add error
+        setErrorFor(name, 'Username cannot be blank');
+	} else {
+        //add success class
+		setSuccessFor(name);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+	} else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value === '') {
+		setErrorFor(password2, 'Password2 cannot be blank');
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Passwords does not match');
+	} else{
+		setSuccessFor(password2);
+	}
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement; //form-control
+	const small = formControl.querySelector('small');
+	//add error message inside small
+	small.innerText = message;
+	//add error class
+	formControl.className = 'form-control error';
+	
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement; 
+
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
